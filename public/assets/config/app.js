7c2fe48e82d6c77f076b5b2f2b3eebc481ec6800
Kyle Seem <kyle.seem@gmail.com>
2018-06-11 12:06:38 -0400

    removed ng-flow packages

--- public/assets/config/app.js
+++ public/assets/config/app.js
@@ -1,2 +1,2 @@
 // define angular application and injectables
-const myApp = angular.module('myApp', ['ngRoute', 'ngCookies', 'ngAnimate', 'ui.bootstrap', 'wu.masonry', 'flow']);
+const myApp = angular.module('myApp', ['ngRoute', 'ngCookies', 'ngAnimate', 'ui.bootstrap', 'wu.masonry']);
